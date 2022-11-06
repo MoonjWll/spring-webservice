11f89d73e5ce1793f9deda87246e60eba7c35917
EC2 Default User <ec2-user@ip-172-31-46-20.ap-northeast-2.compute.internal>
2022-11-06 16:05:05 +0000

    set application.yml

--- src/main/java/com/mwe/webservice/Application.java
+++ src/main/java/com/mwe/webservice/Application.java
@@ -8,8 +8,20 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class Application {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/springboot-webservice/real-application.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class)
+            .properties(APPLICATION_LOCATIONS)
+            .run(args);
+
+        // 스프링부트 프로젝트가 실행될 때 프로젝트 내부 application.yml과 외부 real-application.yml 모두 불러온다.
+
+
+
+        // SpringApplication.run(Application.class, args);
     }
 
 }
