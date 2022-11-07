ef1c0e645c5ff8914e0f83682be1caeec9a4e3e1
MoonjWll <mjw0147@gmail.com>
2022-11-07 12:27:05 +0900

    Set [set1] application

--- src/main/java/com/mwe/webservice/Application.java
+++ src/main/java/com/mwe/webservice/Application.java
@@ -1,7 +1,8 @@
 package com.mwe.webservice;
 
-import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -9,15 +10,16 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Application {
 
     public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.yml,"
-            + "/app/config/springboot-webservice/real-application.yml";
+//            + "classpath:application.yml,";
+//            + "/app/config/spring-webservice/real-application.yml";
+            + "C:\\Users\\mwe61\\app\\config\\spring-webservice\\real-application.yml";
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(Application.class)
             .properties(APPLICATION_LOCATIONS)
             .run(args);
 
-        // 스프링부트 프로젝트가 실행될 때 프로젝트 내부 application.yml과 외부 real-application.yml 모두 불러온다.
+        // 스프링부트 프로젝트가 실행될 때 프로젝트 내부 application.yml 과 외부 real-application.yml 모두 불러온다.
 
 
 
--- src/main/java/com/mwe/webservice/web/WebRestController.java
+++ src/main/java/com/mwe/webservice/web/WebRestController.java
@@ -4,19 +4,30 @@ import com.mwe.webservice.domain.posts.PostsRepository;
 import com.mwe.webservice.dto.posts.PostsSaveRequestDto;
 import com.mwe.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
 
 @RestController
 @AllArgsConstructor
 public class WebRestController {
 
     private PostsService postsService;
+    private Environment env;
 
     @PostMapping("/posts")
     public Long savePosts(@RequestBody PostsSaveRequestDto dto)
     {
         return postsService.save(dto);
 
+    }
+
+    @GetMapping("/profile")
+    public String getProfile () {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 
 
